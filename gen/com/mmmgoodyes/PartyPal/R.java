a8316f307d862d44b8bc6f1216d9d202dfeb18df
Toolbox <jpo3@duke.edu>
2013-11-17 10:32:33 -0500

    UI Changes

--- gen/com/mmmgoodyes/PartyPal/R.java
+++ gen/com/mmmgoodyes/PartyPal/R.java
@@ -63,22 +63,24 @@ public final class R {
         public static final int tab_unselected_pressed_partypal=0x7f020020;
     }
     public static final class id {
-        public static final int Check_In=0x7f090005;
-        public static final int Check_Out=0x7f090004;
-        public static final int Group_List=0x7f090003;
+        public static final int Add_Member=0x7f09000a;
+        public static final int Check_In=0x7f090006;
+        public static final int Check_Out=0x7f090003;
+        public static final int CountDown=0x7f090005;
+        public static final int Group_List=0x7f090004;
         public static final int Group_Name=0x7f090000;
         public static final int Join_Button=0x7f090001;
-        public static final int Name=0x7f09000b;
-        public static final int Number=0x7f09000c;
+        public static final int Missing_title=0x7f090007;
+        public static final int Name=0x7f09000e;
+        public static final int Number=0x7f09000f;
         public static final int Start_Button=0x7f090002;
-        public static final int action_settings=0x7f09000f;
-        public static final int intro_text=0x7f09000a;
-        public static final int quickContactBadge1=0x7f09000e;
-        public static final int start_button=0x7f09000d;
-        public static final int tableRow1=0x7f090006;
-        public static final int tableRow2=0x7f090007;
-        public static final int tableRow3=0x7f090008;
-        public static final int tableRow4=0x7f090009;
+        public static final int Time_Label=0x7f09000b;
+        public static final int action_settings=0x7f090011;
+        public static final int intro_text=0x7f09000d;
+        public static final int start_button=0x7f090010;
+        public static final int tableRow1=0x7f090008;
+        public static final int textView1=0x7f090009;
+        public static final int textView2=0x7f09000c;
     }
     public static final class layout {
         public static final int activity_create__group=0x7f030000;
