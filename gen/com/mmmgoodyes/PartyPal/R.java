d3c2be784aa3793f2086c25bc3311573707b0051
Toolbox <jpo3@duke.edu>
2013-11-17 05:39:21 -0500

    Added Async user addition

--- gen/com/mmmgoodyes/PartyPal/R.java
+++ gen/com/mmmgoodyes/PartyPal/R.java
@@ -63,19 +63,27 @@ public final class R {
         public static final int tab_unselected_pressed_partypal=0x7f020020;
     }
     public static final class id {
-        public static final int action_settings=0x7f090002;
-        public static final int intro_text=0x7f090000;
-        public static final int start_button=0x7f090001;
+        public static final int Join_Button=0x7f090000;
+        public static final int Name=0x7f090003;
+        public static final int Number=0x7f090004;
+        public static final int Start_Button=0x7f090001;
+        public static final int action_settings=0x7f090006;
+        public static final int intro_text=0x7f090002;
+        public static final int start_button=0x7f090005;
     }
     public static final class layout {
-        public static final int activity_dashboard=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_main_menu=0x7f030002;
+        public static final int activity_create__group=0x7f030000;
+        public static final int activity_dashboard=0x7f030001;
+        public static final int activity_join__group=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_main_menu=0x7f030004;
     }
     public static final class menu {
-        public static final int dashboard=0x7f080000;
-        public static final int main=0x7f080001;
-        public static final int main_menu=0x7f080002;
+        public static final int create__group=0x7f080000;
+        public static final int dashboard=0x7f080001;
+        public static final int join__group=0x7f080002;
+        public static final int main=0x7f080003;
+        public static final int main_menu=0x7f080004;
     }
     public static final class string {
         public static final int action_settings=0x7f060001;
@@ -83,7 +91,12 @@ public final class R {
         public static final int hello_world=0x7f060002;
         public static final int intro_buttontext=0x7f060004;
         public static final int intro_text=0x7f060003;
+        public static final int join_group=0x7f060007;
+        public static final int request_URL=0x7f06000b;
+        public static final int start_group=0x7f060008;
+        public static final int title_activity_create__group=0x7f06000a;
         public static final int title_activity_dashboard=0x7f060006;
+        public static final int title_activity_join__group=0x7f060009;
         public static final int title_activity_main_menu=0x7f060005;
     }
     public static final class style {
--- src/com/mmmgoodyes/PartyPal/MainActivity.java
+++ src/com/mmmgoodyes/PartyPal/MainActivity.java
@@ -1,19 +1,52 @@
 package com.mmmgoodyes.PartyPal;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.mmmgoodyes.PartyPal.Dashboard.RequestTask;
+import com.mmmgoodyes.PartyPal.R.id;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.text.Editable;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	public String number;
+	public String name;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		number = "";
+		name = "";
 	}
 
 	@Override
@@ -29,8 +62,67 @@ public class MainActivity extends Activity {
 		Button mButton=(Button)findViewById(R.id.start_button);
 		mButton.setBackgroundColor(Color.parseColor("#454545")); // custom color
 		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		Intent intent = new Intent(this,Dashboard.class);
-		startActivity(intent);
+
+		
+		
+		
+				
+				
+				
+							
+
+
+			
+		
+		
+	
+		number = ((EditText)findViewById(R.id.Number)).getText().toString();
+		name = ((EditText)findViewById(R.id.Name)).getText().toString();
+		
+		
+		new RequestTask().execute("");
+		
+		Log.v("name",name);
+		Log.v("number",number);
+		
+		if (number.length()==10 && name.length()>0) {
+			
+			intent.putExtra("number", number);
+			intent.putExtra("name", name);
+			
+			startActivity(intent);
+
+		}
 	}
 
+	class RequestTask extends AsyncTask<String, String, String>{
+	    @Override
+	    protected String doInBackground(String... uri) {
+	    	
+			Button mButton=(Button)findViewById(R.id.start_button);
+			//mButton.setBackgroundColor(getResources().getColor(R.color.DarkerGray)); // custom color
+
+	    	
+	        return "";
+	    }
+	    
+	    @Override
+	    protected void onPostExecute(String result) {
+	        super.onPostExecute(result);
+	        //Do anything with response..
+	    }
+	}
 }
